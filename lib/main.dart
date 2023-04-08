02a360b6090f170e991671ecf1677fd7fc0f6ccb
gunadermawan <gunadermawan.official@gmail.com>
2023-04-08 21:33:42 +0700

    create sample stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,23 @@ class MyApp extends StatelessWidget {
           title: const Text('Hello World!'),
         ),
         body: const Center(
-          child: Text('Hello World!'),
+          child: Heading(text: 'Hello World!'),
         ),
       ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
     );
   }
 }
