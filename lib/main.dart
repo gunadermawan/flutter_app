63320b3c331124fc709bc5e9a529dd73452d6d69
gunadermawan <gunadermawan.official@gmail.com>
2023-04-08 21:51:04 +0700

    create sample stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Hello World!'),
         ),
         body: const Center(
-          child: Heading(text: 'Hello World!'),
+          child: BiggerText(text: 'Hello World!'),
         ),
       ),
     );
@@ -34,6 +34,36 @@ class Heading extends StatelessWidget {
     return Text(
       text,
       style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _BiggerText();
+}
+
+class _BiggerText extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _textSize = 32.0;
+              });
+            },
+            child: const Text("Perbesar"))
+      ],
     );
   }
 }
